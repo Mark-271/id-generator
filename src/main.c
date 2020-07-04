265b09784e21e1e1f8ec50130b58c1dff0f41afa
Mark Sungurov <mark.sungurov@gmail.com>
2020-07-04 17:09:53 +0300

    Add while loop inside thread_func

--- src/main.c
+++ src/main.c
@@ -2,10 +2,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <pthread.h>
 #include <errno.h>
 
 #define TH_NUM 10
+
+static pthread_t th_id[TH_NUM];
+static int finished;
 
 /* function to generate unique ids */
 int id_gen()
@@ -17,30 +21,33 @@ int id_gen()
 /* function to be operated by thread */
 void *thread_func(void *data)
 {
-	//UNUSED(data);
-	printf("thread %i - id %d\n", *(int *)data, id_gen());
+	UNUSED(data);
+	while (!finished)	
+		printf("%d\n", id_gen());
+	
 	return NULL;
 }
 
 int main(void)
 {
-	pthread_t th_id[TH_NUM];
-	int ret;
+	int err;
 	size_t i;
-	for(i = 0; i < TH_NUM; ++i) {
-		th_id[i] = i;
-		ret = pthread_create(&th_id[i], NULL, thread_func, &i);
-		if (ret) {
+
+	for (i = 0; i < TH_NUM; ++i) {
+		err = pthread_create(&th_id[i], NULL, thread_func, &i);
+		if (err) {
 			perror("Error in pthread_create()");
 			return EXIT_FAILURE;
 		}
 	}
 
 	msleep(5000);
+	finished = true;
 
-	for(i = 0; i < TH_NUM; ++i) {
-		ret = pthread_join(th_id[i], NULL);
-		if (ret) {
+	
+	for (i = 0; i < TH_NUM; ++i) {
+		err = pthread_join(th_id[i], NULL);
+		if (err) {
 			perror("Error in pthread_join()");
 			return EXIT_FAILURE;
 		}	
